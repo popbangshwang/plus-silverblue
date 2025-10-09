f86572ca547c14e6b8a411cd6ec0f548c3c148b0
synq <jurgendarwin@gmail.com>
2025-10-09 14:38:19 +1000

    fixed nvidia-settings-toggle.service to point to /usr/bin, added lsd package in recipe.yml, added ls aliases in config.fish

--- files/system/etc/fish/config.fish
+++ files/system/etc/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     starship init fish | source
+
+    # Alias ls commands to use lsd with icons
+    alias ls 'lsd --icon=auto'
+    alias ll 'lsd -l --icon=auto'
+    alias la 'lsd -la --icon=auto'
 end
