2baac32546e70d5dee08297525b80d307788deab
synq <jurgendarwin@gmail.com>
2025-10-09 20:43:58 +1000

    nvidia .desktop hide attempt 2

--- files/system/usr/bin/toggle-nvidia-settings.sh
+++ files/system/usr/bin/toggle-nvidia-settings.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-SOURCE_DESKTOP="/usr/local/share/applications/nvidia-settings.desktop"
-OVERRIDE_DESKTOP="/etc/xdg/autostart/nvidia-settings.desktop"
+DESKTOP_FILE="/usr/share/applications/nvidia-settings.desktop"
 
-if [ ! -f "$SOURCE_DESKTOP" ]; then
-  echo "Source .desktop file missing."
+if [ ! -f "$DESKTOP_FILE" ]; then
+  echo "Desktop file not found."
   exit 1
 fi
 
-# Copy the base .desktop file to override path
-cp "$SOURCE_DESKTOP" "$OVERRIDE_DESKTOP"
-
 if lspci | grep -qi nvidia; then
   # Show NVIDIA Settings
-  sed -i 's/^NoDisplay=true/NoDisplay=false/' "$OVERRIDE_DESKTOP"
+  sed -i 's/^NoDisplay=true/NoDisplay=false/' "$DESKTOP_FILE"
   echo "NVIDIA GPU detected, un-hiding NVIDIA Settings."
 else
   # Hide it
-  sed -i 's/^NoDisplay=false/NoDisplay=true/' "$OVERRIDE_DESKTOP"
+  sed -i 's/^NoDisplay=false/NoDisplay=true/' "$DESKTOP_FILE"
   echo "No NVIDIA GPU detected, hiding NVIDIA Settings."
 fi
+
